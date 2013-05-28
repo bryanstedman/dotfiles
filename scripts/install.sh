eee8d8d03ab7fd54ce37261e3e0008c2130643fc
Bryan Stedman <bryanbstedman@gmail.com>
2013-05-28 10:14:26 -0700

    Create local aliases file

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,42 +1,27 @@
-#!/bin/bash
-
-########## Variables
-
 dir=$(pwd) # dotfiles directory
 
 echo "Brewing"
 ruby $dir/scripts/brewery.rb
-
-echo "Updating bundles"
-ruby $dir/scripts/vim_bundles.rb $1
 
 echo "~~~~~~~~~~~~~~~~~"
 echo "Symlinking"
 
 #symlinks
 #zsh
+echo "Creating local alias file"
+touch $dir/zsh/local.zsh
+
 echo "Symlinking ZSH"
-ln -nsf $dir/zsh/.zsh ~/.zsh
-ln -sf $dir/zsh/.zshenv ~/.zshenv
 ln -sf $dir/zsh/.zshrc ~/.zshrc
-
-#vim
-echo "Symlinking Vim"
-ln -nsf $dir/vim/.vim ~/.vim
-ln -sf $dir/vim/.vimrc ~/.vimrc
-
-#tmux
-echo "Symlinking tmux"
-ln -sf $dir/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $dir/zsh/aliases.zsh ~/.ohmyzsh/custom/aliases.zsh
+ln -sf $dir/zsh/functions.zsh ~/.ohmyzsh/custom/functions.zsh
+ln -sf $dir/zsh/local.zsh ~/.ohmyzsh/custom/local.zsh
+ln -sf $dir/zsh/hipster.zsh-theme ~/.ohmyzsh/themes/hipster.zsh-theme
 
 #git
 echo "Symlinking git"
 ln -sf $dir/git/.gitconfig ~/.gitconfig
 ln -sf $dir/git/.gitignore_global ~/.gitignore_global
-
-#bin
-echo "Symlinking tmux-vim-select-pane to /usr/bin"
-sudo ln -sf $dir/bin/tmux-vim-select-pane /usr/bin/tmux-vim-select-pane
 
 echo "~~~~~~~~~~~~~~~~~"
 echo "All done"
