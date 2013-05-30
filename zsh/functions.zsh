f7de4cdb3833a68022fffe80537380aca29678f6
Bryan Stedman <bryanbstedman@gmail.com>
2013-05-29 20:13:20 -0700

    Add datauri function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -58,6 +58,15 @@ g () {
   fi
 }
 
+# Create a data URI from a file and copy it to the pasteboard
+datauri() {
+    local mimeType=$(file -b --mime-type "$1")
+    if [[ $mimeType == text/* ]]; then
+        mimeType="${mimeType};charset=utf-8"
+    fi
+    printf "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')" | pbcopy | printf "=> data URI copied to pasteboard.\n"
+}
+
 # SVN log for specific username
 svnuserlog() {
   svn log | sed -n "/$1/,/-----$/ p"
