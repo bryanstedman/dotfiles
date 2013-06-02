92120e493743a7c419331a3fac19e4887466d962
Bryan Stedman <bryan@bryanstedman.com>
2013-06-01 18:09:40 -0700

    Add cask script to install apps

---
+++ scripts/cask.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+# Installs apps with brew-cask
+puts 'Tapping homebrew-cask...'
+puts 'brew tap phinze/homebrew-cask'
+puts 'brew install brew-cask'
+
+apps = %w{
+  alfred
+  google-chrome
+  google-chrome-canary
+  firefox
+  opera
+  iterm2
+  sublime-text
+  github
+  codekit
+  dropbox
+  fantastical
+  vlc
+  spotify
+  bartender
+  things
+  scrivener
+}
+
+puts 'brew cask install alfred'
+
+installed = `brew cask list`
+apps.each do |app|
+  if(installed.include? app)
+    puts "#{app} already installed"
+  else
+    puts "running brew cask install #{app}"
+    puts `brew cask install #{app}`
+  end
+end
+
+# Tell Alfred about cask apps straight away
+puts 'brew cask alfred'
